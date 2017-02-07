d0f8540f6e796de0baf5586d4bb130d6b7e835b2
pq <ybjqx3340@gmail.com>
2017-02-07 13:28:52 -0800

    added test for TCPServer

---
+++ src/test/TestTCPServer.java
@@ -0,0 +1,13 @@
+package test;
+
+import rmi.TCPServer;
+
+/**
+ * A quick test to test if TCPServer and associated classes are implemented correctly.
+ */
+public class TestTCPServer {
+  public void runTest() {
+    TCPServer tcpServer = new TCPServer(8081);
+
+  }
+}
