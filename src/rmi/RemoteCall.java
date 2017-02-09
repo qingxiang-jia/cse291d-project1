8af86937c6921a985c2b6a50985446ff06a34837
unknown <>
2017-02-09 15:58:43 -0800

    aaa

--- src/rmi/RemoteCall.java
+++ src/rmi/RemoteCall.java
@@ -2,21 +2,23 @@ package rmi;
 
 import java.io.Serializable;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 
 public class RemoteCall implements Serializable {
   private Method method;
-  private Object[] args;
+  private List<Object> args;
 
   public RemoteCall(Method method, Object[] args) {
     this.method = method;
-    this.args = args;
+    this.args = Arrays.asList(args);
   }
 
   public Method getMethod() {
     return method;
   }
 
-  public Object[] getArgs() {
+  public List<Object> getArgs() {
     return args;
   }
   
