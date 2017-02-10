1b65d21c3d1a7557086dba099c6a3c211bcc0a8d
pq <ybjqx3340@gmail.com>
2017-02-10 13:23:12 -0800

    passed one more test

--- src/conformance/ConformanceTests.java
+++ src/conformance/ConformanceTests.java
@@ -37,8 +37,8 @@ public class ConformanceTests
                          conformance.rmi.RestartTest.class, // passed
                          conformance.rmi.NoAddressTest.class, // passed
                          conformance.rmi.ServiceErrorTest.class, // passed
-                         conformance.rmi.StubTest.class // passed
-//                         conformance.rmi.EqualsTest.class,
+                         conformance.rmi.StubTest.class, // passed
+                         conformance.rmi.EqualsTest.class
 //                         conformance.rmi.HashCodeTest.class,
 //                         conformance.rmi.ToStringTest.class,
 //                         conformance.rmi.SerializableTest.class,
