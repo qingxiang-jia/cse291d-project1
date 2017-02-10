02189699d4fa87a2f7dd4ba3bc36e596280ef207
pq <ybjqx3340@gmail.com>
2017-02-09 20:38:36 -0800

    again, solved some more bugs

--- src/rmi/RemoteReturn.java
+++ src/rmi/RemoteReturn.java
@@ -6,13 +6,41 @@ import java.util.List;
 
 public class RemoteReturn implements Serializable{
   private List<Object> returnValue;
+  private Class exceptionType;
+  private Boolean hasException;
+  private Throwable exception;
 
   RemoteReturn(Object returnValue) {
     this.returnValue = new ArrayList<>();
     this.returnValue.add(returnValue);
+    hasException = Boolean.FALSE;
   }
 
   public Object getReturnValue() {
     return returnValue.get(0);
+  }
+
+  public Class getExceptionType() {
+    return exceptionType;
+  }
+
+  public void setExceptionType(Class exceptionType) {
+    this.exceptionType = exceptionType;
+  }
+
+  public Boolean getHasException() {
+    return hasException;
+  }
+
+  public void setHasException(Boolean hasException) {
+    this.hasException = hasException;
+  }
+
+  public Throwable getException() {
+    return exception;
+  }
+
+  public void setException(Throwable exception) {
+    this.exception = exception;
   }
 }
--- src/rmi/TCPWorker.java
+++ src/rmi/TCPWorker.java
@@ -49,24 +49,24 @@ public class TCPWorker<T> implements Runnable {
     RemoteCall remoteCall = null;
     Object ret = null;
     if (obj == null) {
-      send(new RMIException("Received null object"));
+      send(new RemoteReturn(new RMIException("Received null object")));
     }
     try {
       remoteCall = (RemoteCall) obj;
     } catch (ClassCastException e) {
-      send(new RMIException("Received object is not an instance of RemoteCall"));
+      send(new RemoteReturn(new RMIException("Received object is not an instance of RemoteCall")));
     }
     if (!validMethodNames.contains(remoteCall.getMethodName())) {
-      send(new RMIException("Received method is not valid"));
+      send(new RemoteReturn(new RMIException("Received method is not valid")));
     }
     if (remoteCall.getParaTypes() == null) {
-      send(new RMIException("Parameter list is null"));
+      send(new RemoteReturn(new RMIException("Parameter list is null")));
     }
     if (remoteCall.getArgs() == null) {
-      send(new RMIException("Argument list is null"));
+      send(new RemoteReturn(new RMIException("Argument list is null")));
     }
     if (remoteCall.getParaTypes().size() != remoteCall.getArgs().size()) {
-      send(new RMIException("Method parameter quantity does not match actual argument quantity"));
+      send(new RemoteReturn(new RMIException("Method parameter quantity does not match actual argument quantity")));
     }
     Class[] parameterTypeArray = new Class[remoteCall.getParaTypes().size()];
     for (int i = 0; i < remoteCall.getParaTypes().size(); i++) {
@@ -75,12 +75,21 @@ public class TCPWorker<T> implements Runnable {
     try {
       Method methodOnRemoteObject = remoteObject.getClass().getMethod(remoteCall.getMethodName(), parameterTypeArray);
       Object[] args = new Object[remoteCall.getArgs().size()];
+      for (int i = 0; i < args.length; i++) {
+        args[i] = remoteCall.getArgs().get(i);
+      }
       ret = methodOnRemoteObject.invoke(remoteObject, args);
     } catch (NoSuchMethodException e) {
       System.out.println("This should never happen");
       e.printStackTrace();
-    } catch (IllegalAccessException | InvocationTargetException e) {
-      send(new RMIException("Remote object failed to execute your remote call"));
+    } catch (IllegalAccessException e) {
+      send(new RemoteReturn(new RMIException("Remote object failed to execute your remote call")));
+    } catch (InvocationTargetException e) {
+      RemoteReturn rRet = new RemoteReturn(ret);
+      rRet.setHasException(Boolean.TRUE);
+      rRet.setException(e.getTargetException());
+      rRet.setExceptionType(e.getTargetException().getClass());
+      send(rRet);
     }
     send(new RemoteReturn(ret));
   }
