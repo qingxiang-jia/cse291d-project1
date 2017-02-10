7dbf80e1f31936dac9688f3dd1de35515ed56c87
unknown <>
2017-02-09 21:28:33 -0800

    aa

--- src/conformance_old/ConformanceTests.java
+++ src/conformance_old/ConformanceTests.java
@@ -24,29 +24,7 @@ public class ConformanceTests
         // Create the test list, the series object, and run the test series.
         @SuppressWarnings("unchecked")
         Class<? extends Test>[]     tests =
-            new Class[] {conformance_old.rmi.CallTest.class,
-                         conformance_old.rmi.ArgumentTest.class,
-                         conformance_old.rmi.ReturnTest.class,
-                         conformance_old.rmi.ExceptionTest.class,
-                         conformance_old.rmi.CompleteCallTest.class,
-                         conformance_old.rmi.ImplicitStubCallTest.class,
-                         conformance_old.rmi.NullTest.class,
-                         conformance_old.rmi.RemoteInterfaceTest.class,
-                         conformance_old.rmi.ListenTest.class,
-                         conformance_old.rmi.RestartTest.class,
-                         conformance_old.rmi.NoAddressTest.class,
-                         conformance_old.rmi.ServiceErrorTest.class,
-                         conformance_old.rmi.StubTest.class,
-                         conformance_old.rmi.EqualsTest.class,
-                         conformance_old.rmi.HashCodeTest.class,
-                         conformance_old.rmi.ToStringTest.class,
-                         conformance_old.rmi.SerializableTest.class,
-                         conformance_old.rmi.OverloadTest.class,
-                         conformance_old.rmi.ShadowTest.class,
-                         conformance_old.rmi.InheritanceTest.class,
-                         conformance_old.rmi.SubclassTest.class,
-                         conformance_old.rmi.SecurityTest.class,
-                         conformance_old.rmi.ThreadTest.class};
+            new Class[] {conformance_old.rmi.ThreadTest.class};
 
         Series                      series = new Series(tests);
         SeriesReport                report = series.run(3, System.out);
